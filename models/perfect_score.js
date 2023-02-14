5c473348fd680a34ee52a33204ae4c70ce4a9023
indoor98 <98157935+indoor98@users.noreply.github.com>
2023-02-14 17:14:43 +0900

    데이터베이스 모델 변경

---
+++ models/perfect_score.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Perfect = sequelize.define('perfect', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+    college: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numOfPerfect: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
