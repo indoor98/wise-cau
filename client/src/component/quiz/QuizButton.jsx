83d6a6be8ab73b15778ccbc66973826889567879
lims00 <limsoo@naver.com>
2023-02-19 00:14:40 +0900

    quiz page 미완1!

---
+++ client/src/component/quiz/QuizButton.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const Button = styled.div`
+  
+  margin-top:10px;
+  background-color: #ffffff;
+  border:1px solid grey;
+  border-radius: 5px;
+  width: 250px;
+  height:68px;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-align: left;
+  display: flex;
+  
+
+
+  
+`
+const ButtonMargin=styled.div`
+  margin: auto 0;
+`;
+function QuizButton(props){
+    const {onClick,index,text}=props;
+
+    return(
+
+        <Button onClick={onClick}>
+            <ButtonMargin>{index}. {text}</ButtonMargin>
+        </Button>
+    );
+
+}
+
+export default QuizButton;
--- client/src/page/QuizPage.jsx
+++ client/src/page/QuizPage.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 import QuizImg from '../푸앙_독서.png';
 
 import DefaultWrapper from "../component/ui/DefaultWrapper";
 import QuizQuestion from "../component/quiz/QuizQuestion";
 import VerticalAlignCenterWrapper from "../component/ui/VerticalAlignCenterWrapper";
-import {VERSION} from "axios";
-
+import axios, {VERSION} from "axios";
+import QuizButton from "../component/quiz/QuizButton";
 
 
 function QuizPage() {
-    
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/quizes').then(response => {
+        });
+    }, []);
+
     return (
         <DefaultWrapper>
-                    <QuizQuestion num='01' question="문제입니다."/>
+                    <QuizQuestion num='01' question="(서울) 다음 중 중앙도서관 3층 아고라존에 대한 설명으로 옳지 않은 것을 고르시오."/>
                     <img src={QuizImg} style={{width:130,paddingLeft : 180}}/>
-                    <div style={{paddingTop:20,paddingRight:200}}>
-                        <div>답안1</div>
-                        <div>답안2</div>
-                        <div>답안3</div>
-                        <div>답안4</div>
+                    <div>
+                        <QuizButton index='A' text="강의평가를 하지 않아도 성적 이의 신청 기간이 지나면 성적을 확인 할 수 있다."></QuizButton>
+                        <QuizButton index='A' text="성적을 확인 할 수 있다."></QuizButton>
+                        <QuizButton index='A' text="강의평가를 하지 않아도 성적 이의 신청 기간이 지나면 성적"></QuizButton>
+                        <QuizButton index='A' text="강의평가를 하지 않아도 성적 이의 신청 기간이 지나면 성적을 확인 할 수 있다."></QuizButton>
+
                     </div>
         </DefaultWrapper>
 
