29da325a7485142bd08e1a59b26b8d388f67b55f
lims00 <limsoo@naver.com>
2023-02-20 20:24:17 +0900

    다음문제 버튼 삽입했으나 크기 조절 할예정...?

--- client/src/page/QuizPage.jsx
+++ client/src/page/QuizPage.jsx
@@ -7,6 +7,7 @@ import VerticalAlignCenterWrapper from "../component/ui/VerticalAlignCenterWrapp
 import axios, {VERSION} from "axios";
 import QuizButton from "../component/quiz/QuizButton";
 import QuizSolution from "../component/quiz/QuizSolution";
+import DefaultButton from "../component/ui/DefaultButton";
 
 function QuizPage() {
     useEffect(() => {
@@ -25,7 +26,7 @@ function QuizPage() {
                         <QuizButton index='A' text="강의평가를 하지 않아도 성적 이의 신청 기간이 지나면 성적을 확인 할 수 있다."></QuizButton>
                     </div>
                     <QuizSolution solution="중앙 자랑이 아닌 중앙 사랑 장학금이다. 친가족이 2명 이상 본 대학교에 재학 또는 휴학하고 있는 경우 재학중인 1인에게 장학금을 지원하고 있다. 8분위 이내의 학생은 근로를 통해 장학금을 받을 수 있다."/>
-
+                    <DefaultButton title="다음 문제"/>
         </DefaultWrapper>
 
     );
