81f1781bb821163b2d3cfa661ee5ed7ae581b147
lims00 <limsoo@naver.com>
2023-02-25 21:06:55 +0900

    비정상 접근시 단과대 선택으로 리다이렉팅+로딩중 로더 컴포넌트 띄우기

--- client/src/page/QuizPage.jsx
+++ client/src/page/QuizPage.jsx
@@ -10,22 +10,31 @@ import QuizSolution from "../component/quiz/QuizSolution";
 import QuizNextButton from "../component/quiz/QuizNextButton";
 import QuizPageNum from "../component/quiz/QuizPageNum";
 import QuizConverter from "../util/QuizConverter";
-
+import {useNavigate} from "react-router-dom";
+import Loader from "../component/ui/Loader";
+import ErrorUtil from "../util/ErrorUtil";
+import Error from "../component/ui/Error";
+import DefaultButton from "../component/ui/DefaultButton";
+import VerticalAlignCenterWrapper from "../component/ui/VerticalAlignCenterWrapper";
 let score=0;
 function QuizPage() {
-
-
-
-    useEffect(() => {
-        axios.get('/api/quizzes').then(response => {
-            console.log(response.data.result);
-            setDataState(QuizConverter.convert(response.data.result));
-        });
-    }, []);
+    const navigate = useNavigate();
     const [pageState,setPageState] = useState(false);
     const [dataState,setDataState]=useState(null);
     const [answerState,setAnswerState]=useState(0);
     const [pageNumState,setPageNumState]=useState(0);
+    const [isLoading,setIsLoading]=useState(true);
+    const [networkError, setNetworkError] = useState(ErrorUtil.OK);
+    useEffect(() => {
+        axios.get('/api/quizzes').then(response => {
+            console.log(response.data.result);
+            setDataState(QuizConverter.convert(response.data.result));
+            setIsLoading(false);
+        }).catch(error=>{
+            setNetworkError(ErrorUtil.NETWORK_ERROR);
+        });
+    }, []);
+
     const onOptionClick = (event) => {
         if (pageState===true){
             return ``
@@ -38,14 +47,14 @@ function QuizPage() {
         if (choose==dataState[pageNumState].answer){
             setAnswerState(1);
             chooseButton.style.backgroundColor='#b0ffa3';
+            chooseButton.style.border='3px solid green';
             score+=1;
-            console.log(score);
 
         }
         else{
             setAnswerState(2);
             chooseButton.style.border='3px solid lightcoral';
-            answerElement.style.backgroundColor='#9dff9a';
+            answerElement.style.backgroundColor='#b0ffa3';
             answerElement.style.border='3px solid green';
 
         }
@@ -61,26 +70,52 @@ function QuizPage() {
         let array=['0','1','2','3'];
         array.map(function (i){
             document.getElementById(i).style.backgroundColor='#ffffff';
-            document.getElementById(i).style.border='3px solid #143365';
+            document.getElementById(i).style.border='3px solid grey';
         });
 
     }
+    const onResultClick=()=>{
+        navigate('/result',{state:{}});
+
+    }
+    const backClickHandler = () => {
+        navigate('/');
+    }
+
+    let quizContent= isLoading?
+
+        <Loader/>
+
+        :
+        <>
+            <QuizQuestion num={pageNumState+1} question={dataState===null? 'data is null':dataState[pageNumState].title}/>
+            <img src={pageState? (answerState===1 ? CorrectImg:WrongImg) : QuizImg} style={{width:130,paddingLeft : 180}}/>
+            <div >
+                <QuizOptionButton onClick={onOptionClick} id='0' index='A' text={dataState===null? 'data is null':dataState[pageNumState].option[0]}></QuizOptionButton>
+                <QuizOptionButton onClick={onOptionClick} id='1' index='B' text={dataState===null? 'data is null':dataState[pageNumState].option[1]}></QuizOptionButton>
+                <QuizOptionButton onClick={onOptionClick} id='2' index='C' text={dataState===null? 'data is null':dataState[pageNumState].option[2]}></QuizOptionButton>
+                <QuizOptionButton onClick={onOptionClick} id='3' index='D' text={dataState===null? 'data is null':dataState[pageNumState].option[3]}></QuizOptionButton>
+            </div>
+            {pageState&&<QuizSolution solution={dataState===null? 'data is null':dataState[pageNumState].solution}/>}
+
+            {pageNumState!=9 && pageState && <QuizNextButton onClick={onNextClick} title="다음 문제"/>}
+            {pageNumState==9 && pageState && <QuizNextButton onClick={onResultClick} title="결과 확인"/>}
+            {pageState && <QuizPageNum num={pageNumState+1}/>}
+        </>;
+
+    quizContent=networkError.isError?
+        <div style={{textAlign: 'center',margin:'180px'}}>
+            <Error error={networkError}/>
+            <DefaultButton title='이전으로' onClick={backClickHandler}/>
+        </div>
+        :
+        quizContent;
+
 
 
     return (
         <DefaultWrapper>
-                    <QuizQuestion num={pageNumState+1} question={dataState===null? 'data is null':dataState[pageNumState].title}/>
-                    <img src={pageState? (answerState===1 ? CorrectImg:WrongImg) : QuizImg} style={{width:130,paddingLeft : 180}}/>
-                    <div >
-                        <QuizOptionButton onClick={onOptionClick} id='0' index='A' text={dataState===null? 'data is null':dataState[pageNumState].option[0]}></QuizOptionButton>
-                        <QuizOptionButton onClick={onOptionClick} id='1' index='B' text={dataState===null? 'data is null':dataState[pageNumState].option[1]}></QuizOptionButton>
-                        <QuizOptionButton onClick={onOptionClick} id='2' index='C' text={dataState===null? 'data is null':dataState[pageNumState].option[2]}></QuizOptionButton>
-                        <QuizOptionButton onClick={onOptionClick} id='3' index='D' text={dataState===null? 'data is null':dataState[pageNumState].option[3]}></QuizOptionButton>
-                    </div>
-                    {pageState&&<QuizSolution solution={dataState===null? 'data is null':dataState[pageNumState].solution}/>}
-
-            <QuizNextButton onClick={onNextClick} title="다음 문제"/>
-            <QuizPageNum num={pageNumState+1}/>
+            {quizContent}
         </DefaultWrapper>
 
 
