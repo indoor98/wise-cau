24bae3a25a5dbe276a5b28d25a47d2655815b847
indoor98 <98157935+indoor98@users.noreply.github.com>
2023-02-25 13:19:13 +0900

    경로변경

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const dummy = require('./util/dummyData');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,9 +31,9 @@ app.use(errorController.get404);
 sequelize
   .sync()
   .then(result => {
-    // 테스트용 더미데이터 생성코드 한번만 실행해주시면 될 것 같습니다.
-    // dummy.collegeDummy();
-    // dummy.quizDummy();
+    //테스트용 더미데이터 생성코드 한번만 실행해주시면 될 것 같습니다.
+    dummy.collegeDummy();
+    dummy.quizDummy();
     return result;
   })
   .then(result => {
