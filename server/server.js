4ebd0e03421b7fa9f4f2883efbda350bfab2f34b
indoor98 <98157935+indoor98@users.noreply.github.com>
2023-02-25 14:05:47 +0900

    요청이 들어올때마다 log에 남도록 변경

--- server/server.js
+++ server/server.js
@@ -20,6 +20,9 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //api
+app.use('/', (res, req, next) => {
+  console.log(res);
+})
 app.get('/api/quizzes', quizController.getTenQuizzes);
 app.post('/api/results', collegeController.increseScore);
 app.get('/api/ranking', collegeController.collegeRanking);
@@ -32,9 +35,9 @@ app.use(errorController.get404);
 sequelize
   .sync()
   .then(result => {
-    //테스트용 더미데이터 생성코드 한번만 실행해주시면 될 것 같습니다.
-    dummy.collegeDummy();
-    dummy.quizDummy();
+    // 테스트용 더미데이터 생성코드 한번만 실행해주시면 될 것 같습니다.
+    // dummy.collegeDummy();
+    // dummy.quizDummy();
     return result;
   })
   .then(result => {
