665d0546799345f79f8daf480535f3b6d3c17f77
indoor98 <98157935+indoor98@users.noreply.github.com>
2023-02-26 19:33:30 +0900

    sequelize 쿼리문 로그 안뜨게 변경

--- server/util/database.js
+++ server/util/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize('wise_cau', 'admin', 'wisecau1', {
     dialect: 'mysql',
     host: 'wisecaudb.c77bebzwxj1x.ap-northeast-2.rds.amazonaws.com'
-});
+}, {logging: false});
 
 
 
