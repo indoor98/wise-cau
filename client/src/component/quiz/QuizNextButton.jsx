9e60835ecbdad3b8730bab8b734c19fb4d370b2c
lims00 <limsoo@naver.com>
2023-02-26 20:58:32 +0900

    퀴즈 버튼 radius 조절

--- client/src/component/quiz/QuizNextButton.jsx
+++ client/src/component/quiz/QuizNextButton.jsx
@@ -12,12 +12,13 @@ const Quiz_Button = styled.button`
 
   font-family: 'MICEGothic Bold', serif;
 `
-function QuizNextButton(props) {
-    const {onClick, title} = props;
 
+function QuizNextButton(props) {
+    const {onNextButtonClick,onResultButtonClick,pageNum} = props;
+    const isLastQuiz = pageNum===9;
     return (
-        <Quiz_Button onClick={onClick}>
-            {title}
+        <Quiz_Button onClick={isLastQuiz? onResultButtonClick:onNextButtonClick}>
+            {isLastQuiz? '결과 확인':'다음 문제'}
         </Quiz_Button>
     );
 }
--- client/src/page/QuizPage.jsx
+++ client/src/page/QuizPage.jsx
@@ -16,14 +16,19 @@ import Loader from "../component/ui/Loader";
 import ErrorUtil from "../util/ErrorUtil";
 import Error from "../component/ui/Error";
 import DefaultButton from "../component/ui/DefaultButton";
-import VerticalAlignCenterWrapper from "../component/ui/VerticalAlignCenterWrapper";
+import quizOptionButton from "../component/quiz/QuizOptionButton";
 
+const SelectState = {
+    UNSELECTED: 0,
+    CORRECT: 1,
+    WRONG: 2
+};
 function QuizPage() {
     const navigate = useNavigate();
-    const location=useLocation();
+    const location = useLocation();
     const [pageState,setPageState] = useState(false);
-    const [dataState,setDataState]=useState(null);
-    const [answerState,setAnswerState]=useState(0);
+    const [quizState,setQuizState]=useState(null);
+    const [answerState,setAnswerState]=useState(SelectState.UNSELECTED);
     const [pageNumState,setPageNumState]=useState(0);
     const [isLoading,setIsLoading]=useState(true);
     const [networkError, setNetworkError] = useState(ErrorUtil.OK);
@@ -31,7 +36,7 @@ function QuizPage() {
     useEffect(() => {
         axios.get('/api/quizzes').then(response => {
             console.log(response.data.result);
-            setDataState(QuizConverter.convert(response.data.result));
+            setQuizState(QuizConverter.convert(response.data.result));
             setIsLoading(false);
         }).catch(error=>{
             setNetworkError(ErrorUtil.NETWORK_ERROR);
@@ -45,22 +50,19 @@ function QuizPage() {
         setPageState(true);
         const choose=event.currentTarget.id;
         const chooseButton=document.getElementById(choose);
-        const answerElement=document.getElementById(dataState[pageNumState].answer);
+        const answerElement=document.getElementById(quizState[pageNumState].answer);
 
-        if (choose==dataState[pageNumState].answer){
-            setAnswerState(1);
-            chooseButton.style.backgroundColor='#b0ffa3';
-            chooseButton.style.border='3px solid green';
+        if (choose==quizState[pageNumState].answer){
+            setAnswerState(SelectState.CORRECT);
             setScoreState(scoreState+1);
 
         }
         else{
-            setAnswerState(2);
+            setAnswerState(SelectState.WRONG);
             chooseButton.style.border='3px solid lightcoral';
-            answerElement.style.backgroundColor='#b0ffa3';
-            answerElement.style.border='3px solid green';
         }
-
+        answerElement.style.backgroundColor='#b0ffa3';
+        answerElement.style.border='3px solid green';
     }
     const {state}=location;
     const onNextClick = () => {
@@ -68,9 +70,10 @@ function QuizPage() {
             return ``
         }
         setPageState(false);
-        setAnswerState(0);
+        setAnswerState(SelectState.UNSELECTED);
         setPageNumState(pageNumState + 1);
         let array = ['0', '1', '2', '3'];
+
         array.map(function (i) {
             document.getElementById(i).style.backgroundColor = '#ffffff';
             document.getElementById(i).style.border = '3px solid grey';
@@ -91,23 +94,23 @@ function QuizPage() {
     const backClickHandler = () => {
         navigate('/');
     }
-
+    const indexArray=['A','B','C','D'];
     let quizContent= isLoading?
         <Loader/>
         :
         <>
-            <QuizQuestion num={pageNumState+1} question={dataState===null? 'data is null':dataState[pageNumState].title}/>
-            <img src={pageState? (answerState===1 ? CorrectImg:WrongImg) : QuizImg} style={{width:130,paddingLeft : 180}}/>
-            <div >
-                <QuizOptionButton onClick={onOptionClick} id='0' index='A' text={dataState===null? 'data is null':dataState[pageNumState].option[0]}></QuizOptionButton>
-                <QuizOptionButton onClick={onOptionClick} id='1' index='B' text={dataState===null? 'data is null':dataState[pageNumState].option[1]}></QuizOptionButton>
-                <QuizOptionButton onClick={onOptionClick} id='2' index='C' text={dataState===null? 'data is null':dataState[pageNumState].option[2]}></QuizOptionButton>
-                <QuizOptionButton onClick={onOptionClick} id='3' index='D' text={dataState===null? 'data is null':dataState[pageNumState].option[3]}></QuizOptionButton>
-            </div>
-            {pageState&&<QuizSolution solution={dataState===null? 'data is null':dataState[pageNumState].solution}/>}
+            <QuizQuestion num={pageNumState+1} question={quizState[pageNumState].title}/>
+            <img src={pageState? (answerState===SelectState.CORRECT ? CorrectImg:WrongImg) : QuizImg} style={{width:130,paddingLeft : 180}}/>
+            <div>
+                {
+                    quizState[pageNumState].option.map((op,index)=>{
+                        return <QuizOptionButton onClick={onOptionClick} id={index} index={indexArray[index]} text={op}></QuizOptionButton>
+                        })
+                }
+               </div>
+            {pageState&&<QuizSolution solution={quizState[pageNumState].solution}/>}
 
-            {pageNumState != 9 && pageState && <QuizNextButton onClick={onNextClick} title="다음 문제"/>}
-            {pageNumState == 9 && pageState && <QuizNextButton onClick={onResultClick} title="결과 확인"/>}
+            {pageState && <QuizNextButton onNextButtonClick={onNextClick} onResultButtonClick={onResultClick} pageNum={pageNumState}/>}
             {pageState && <QuizPageNum num={pageNumState+1}/>}
         </>;
 
