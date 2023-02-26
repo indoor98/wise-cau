788f308ebcf118de19abd91c95c397a3ff721da9
lims00 <limsoo@naver.com>
2023-02-26 19:44:14 +0900

    퀴즈 버튼 radius 조절

--- client/src/component/quiz/QuizOptionButton.jsx
+++ client/src/component/quiz/QuizOptionButton.jsx
@@ -5,7 +5,7 @@ const Button = styled.div`
   margin-top:10px;
   background-color: #ffffff;
   border:3px solid grey;
-  border-radius: 5px;
+  border-radius: 15px;
   width: 250px;
   height:68px;
   padding-left: 10px;
