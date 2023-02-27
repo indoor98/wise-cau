afaf0b3540bd12d3def6f68184380faac767e4d2
indoor98 <98157935+indoor98@users.noreply.github.com>
2023-02-27 15:10:46 +0900

    퀴즈 데이터 추가(한번 서버 재실행 후 삭제 예정)

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const sequelize = require('./util/database');
 const path = require('path');
 const Quiz = require('./models/quiz');
 const College = require('./models/college');
+const fs = require('fs');
 
 const collegeController = require('./controller/collegeController');
 const quizController = require('./controller/quizController');
@@ -49,6 +50,53 @@ app.use(function (req, res, next) {
     next();
 });
 
+// 퀴즈 데이터 생성코드(삭제 예정)
+function csvToJSON(csv_string){
+
+  // 1. 문자열을 줄바꿈으로 구분 => 배열에 저장
+  const rows = csv_string.split("\r\n");
+  
+  // 줄바꿈을 \n으로만 구분해야하는 경우, 아래 코드 사용
+  // const rows = csv_string.split("\n");
+
+
+  // 2. 빈 배열 생성: CSV의 각 행을 담을 JSON 객체임
+  const jsonArray = [];
+
+
+  // 3. 제목 행 추출 후, 콤마로 구분 => 배열에 저장
+  const header = rows[0].split(",");
+
+
+  // 4. 내용 행 전체를 객체로 만들어, jsonArray에 담기
+  for(let i = 1; i < rows.length; i++){
+
+      // 빈 객체 생성: 각 내용 행을 객체로 만들어 담아둘 객체임
+      let obj = {};
+
+      // 각 내용 행을 콤마로 구분
+      let row = rows[i].split(",");
+
+      // 각 내용행을 {제목1:내용1, 제목2:내용2, ...} 형태의 객체로 생성
+      for(let j=0; j < header.length; j++){
+          obj[header[j]] = row[j];
+      }
+
+      // 각 내용 행의 객체를 jsonArray배열에 담기
+      jsonArray.push(obj);
+
+  }
+  
+  // 5. 완성된 JSON 객체 배열 반환
+  return jsonArray;
+
+  // 문자열 형태의 JSON으로 반환할 경우, 아래 코드 사용
+  // return JSON.stringify(jsonArray);
+}
+const csvPath = path.join(__dirname, 'quizzes.csv');
+const csv = fs.readFileSync(csvPath, 'utf-8');
+quizzesJSON = csvToJSON(csv);
+
 
 
 /* 에러 발생함
@@ -73,7 +121,7 @@ sequelize
   .then(result => {
     // 테스트용 더미데이터 생성코드 한번만 실행해주시면 될 것 같습니다.
     // dummy.collegeDummy();
-    // dummy.quizDummy();
+    dummy.quizDummy(quizzesJSON);
     return result;
   })
   .then(result => {
